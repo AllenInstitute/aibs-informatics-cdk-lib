d9ef506f93d9c4f9087afae6cb01d36b99f3d651
Ryan McGinty <rpmcginty@users.noreply.github.com>
2023-10-03 11:57:28 -0700

    adding get_env_base used in cdk infra

--- src/aibs_informatics_cdk_lib/project/utils.py
+++ src/aibs_informatics_cdk_lib/project/utils.py
@@ -1,6 +1,25 @@
-__all__ = ["get_package_root"]
+__all__ = [
+    "get_package_root",
+    "get_env_base",
+]
 
+import logging
 import pathlib
+from typing import List, Optional
+
+import constructs
+from aibs_informatics_core.env import (
+    ENV_BASE_KEY_ALIAS,
+    ENV_LABEL_KEY_ALIAS,
+    ENV_TYPE_KEY_ALIAS,
+    LABEL_KEY,
+    LABEL_KEY_ALIAS,
+    EnvBase,
+    EnvType,
+)
+from aibs_informatics_core.utils.os_operations import get_env_var
+
+logger = logging.getLogger(__name__)
 
 
 def get_package_root() -> str:
@@ -19,3 +38,74 @@ def get_package_root() -> str:
             )
         )
     )
+
+
+ENV_BASE_KEYS = [EnvBase.ENV_BASE_KEY, ENV_BASE_KEY_ALIAS, "env"]
+ENV_TYPE_KEYS = [EnvBase.ENV_TYPE_KEY, ENV_TYPE_KEY_ALIAS]
+ENV_LABEL_KEYS = [EnvBase.ENV_LABEL_KEY, ENV_LABEL_KEY_ALIAS, LABEL_KEY, LABEL_KEY_ALIAS]
+
+
+def get_env_base(node: constructs.Node) -> EnvBase:
+    """Resolves EnvBase from cdk context or environment
+
+    Order of resolution:
+        1. CDK context value (specifying -c K=V)
+            1. look for EnvBase
+            2. look for EnvType/EnvLabel
+        2. env variable
+            1. Look for EnvBase
+            2. Look for EnvType/EnvLabel
+
+    Args:
+        node (constructs.Node): cdk construct node
+
+    Returns:
+        EnvBase: environment base
+    """
+    # We need to check from context node FIRST before reading from env variables
+    env_base_str = _get_from_context(node, ENV_BASE_KEYS)
+    env_type_str = _get_from_context(node, ENV_TYPE_KEYS)
+    env_label__from_context = _get_from_context(node, ENV_LABEL_KEYS)
+    env_label__from_env = EnvBase.load_env_label__from_env()
+    if env_base_str:
+        env_base = EnvBase(env_base_str)
+        logger.info(f"Loading EnvBase from CDK CONTEXT: env_base={env_base}")
+        return env_base
+
+    if env_type_str:
+        env_type = EnvType(env_type_str)
+        env_label = env_label__from_context or env_label__from_env
+        env_base = EnvBase.from_type_and_label(env_type=env_type, env_label=env_label)
+        logger.info(f"Loading EnvBase from type/label CDK CONTEXT: env_base={env_base}")
+        return env_base
+
+    # Now we try to load env base from environment
+    try:
+        env_base = EnvBase.from_env()
+        logger.info(f"Loading EnvBase from ENV_VARs: env_base={env_base}")
+        return env_base
+    except:
+        env_type = EnvType.DEV
+
+        env_label = env_label__from_context
+        if env_label is None:
+            if env_label__from_env:
+                env_label = env_label__from_env
+            else:
+                env_label = (get_env_var("USER") or "anonymous").split(".")[0]
+        logger.info(
+            f"No EnvBase defaults set by CDK CONTEXT or ENV VAR. "
+            f"Using env_type={env_type}, env_label={env_label}"
+        )
+        return EnvBase.from_type_and_label(env_type=env_type, env_label=env_label)
+
+
+def _get_from_context(
+    node: constructs.Node, keys: List[str], default: str = None
+) -> Optional[str]:
+    for key in keys:
+        value = node.try_get_context(key)
+        if value is not None:
+            return value
+    else:
+        return default
---
+++ test/aibs_informatics_cdk_lib/project/test_utils.py
@@ -0,0 +1,107 @@
+import aws_cdk as cdk
+import constructs
+import pytest
+from aibs_informatics_core.env import (
+    ENV_BASE_KEY,
+    ENV_BASE_KEY_ALIAS,
+    ENV_LABEL_KEY,
+    ENV_LABEL_KEY_ALIAS,
+    ENV_TYPE_KEY,
+    ENV_TYPE_KEY_ALIAS,
+    LABEL_KEY,
+    LABEL_KEY_ALIAS,
+    EnvBase,
+    EnvType,
+)
+
+from aibs_informatics_cdk_lib.project.utils import (
+    ENV_BASE_KEYS,
+    ENV_LABEL_KEYS,
+    ENV_TYPE_KEYS,
+    get_env_base,
+    get_package_root,
+)
+
+USER = "marmotdev"
+
+
+@pytest.fixture(scope="function")
+def dummy_node():
+    app = cdk.App(analytics_reporting=False, auto_synth=False)
+    construct = constructs.Construct(app, "construct")
+    node = construct.node
+    return node
+
+
+@pytest.fixture(scope="function")
+def env_vars(monkeypatch):
+    for key in ENV_BASE_KEYS + ENV_TYPE_KEYS + ENV_LABEL_KEYS:
+        monkeypatch.delenv(key, raising=False)
+    monkeypatch.setenv("USER", USER)
+    return monkeypatch
+
+
+def test__get_env_base__no_context_no_env_vars(env_vars, dummy_node):
+    assert get_env_base(dummy_node) == EnvBase("dev-marmotdev")
+
+    env_vars.setenv("USER", "marmot.dev")
+    assert get_env_base(dummy_node) == EnvBase("dev-marmot")
+
+
+def test__get_env_base__env_vars_only(env_vars, dummy_node):
+    # Label env var priority tests
+    for i, key in enumerate([LABEL_KEY_ALIAS, LABEL_KEY, ENV_LABEL_KEY_ALIAS, ENV_LABEL_KEY]):
+        label = f"marmot{i}"
+        env_vars.setenv(key, label)
+        assert get_env_base(dummy_node) == EnvBase(f"dev-{label}")
+    env_vars.setenv(ENV_LABEL_KEY, "marmot")
+
+    # Type env var priority tests
+    env_vars.setenv(ENV_TYPE_KEY_ALIAS, "prod")
+    assert get_env_base(dummy_node) == EnvBase("prod-marmot")
+    env_vars.setenv(ENV_TYPE_KEY, "test")
+    assert get_env_base(dummy_node) == EnvBase("test-marmot")
+
+    # Base env var priority tests
+    env_vars.setenv(ENV_BASE_KEY_ALIAS, "dev")
+    assert get_env_base(dummy_node) == EnvBase("dev")
+
+    env_vars.setenv(ENV_BASE_KEY, "prod")
+    assert get_env_base(dummy_node) == EnvBase("prod")
+
+
+def test__get_env_base__context_only(env_vars, dummy_node):
+    # Label env var priority tests
+    for i, key in enumerate([LABEL_KEY_ALIAS, LABEL_KEY, ENV_LABEL_KEY_ALIAS, ENV_LABEL_KEY]):
+        label = f"marmot{i}"
+        dummy_node.set_context(key, label)
+        assert get_env_base(dummy_node) == EnvBase(f"dev-{label}")
+    dummy_node.set_context(ENV_LABEL_KEY, "marmot")
+
+    # Type env var priority tests
+    dummy_node.set_context(ENV_TYPE_KEY_ALIAS, "prod")
+    assert get_env_base(dummy_node) == EnvBase("prod-marmot")
+    dummy_node.set_context(ENV_TYPE_KEY, "test")
+    assert get_env_base(dummy_node) == EnvBase("test-marmot")
+
+    # Base env var priority tests
+    dummy_node.set_context("env", "dev")
+    assert get_env_base(dummy_node) == EnvBase("dev")
+    dummy_node.set_context(ENV_BASE_KEY, "prod")
+    assert get_env_base(dummy_node) == EnvBase("prod")
+
+
+def test__get_env_base__context_and_env_vars(env_vars, dummy_node):
+    # Type/Label not used from context if both not present
+    dummy_node.set_context(ENV_LABEL_KEY, "marmot")
+    env_vars.setenv(ENV_TYPE_KEY_ALIAS, "prod")
+    assert get_env_base(dummy_node) == EnvBase("prod")
+
+    # Type/Label from context supercedes env base from env
+    dummy_node.set_context(ENV_TYPE_KEY, "test")
+    env_vars.setenv(ENV_BASE_KEY_ALIAS, "dev")
+    assert get_env_base(dummy_node) == EnvBase("test-marmot")
+
+    # Base from context supercedes type/label
+    dummy_node.set_context(ENV_BASE_KEY, "dev")
+    assert get_env_base(dummy_node) == EnvBase("dev")
