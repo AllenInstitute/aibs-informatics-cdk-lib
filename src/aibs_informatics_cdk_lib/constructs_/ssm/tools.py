4fb4b6bd6e2c89214f1c8e9f9e2282cc81894ed8
Ryan McGinty <rpmcginty@users.noreply.github.com>
2024-05-14 15:40:25 -0700

    misc fixes

--- src/aibs_informatics_cdk_lib/constructs_/ssm/tools.py
+++ src/aibs_informatics_cdk_lib/constructs_/ssm/tools.py
@@ -31,7 +31,11 @@ class SSMTools(EnvBaseConstruct):
         if destination_key_prefix is None:
             destination_key_prefix = S3KeyPrefix(self.CODE_ASSETS_PREFIX)
 
-        parameter_name = self.env_base.get_ssm_param_name(param_name)
+        if param_name:
+            parameter_name = self.env_base.get_ssm_param_name(param_name)
+        else:
+            parameter_name = self.env_base.get_ssm_param_name(*param_name_components)
+
         filename = f"{asset_name}-asset.zip"
 
         s3deployment.BucketDeployment(
@@ -85,9 +89,7 @@ class SSMTools(EnvBaseConstruct):
         )
 
         # Store the S3 URI location in an SSM parameter
-        s3_uri = S3URI.build(
-            destination_bucket.bucket_name, destination_key, full_validate=False
-        )
+        s3_uri = S3URI.build(destination_bucket.bucket_name, destination_key, full_validate=False)
 
         parameter_name = self.env_base.get_ssm_param_name(*param_name_components)
         cmd_wrapper_param = ssm.StringParameter(
