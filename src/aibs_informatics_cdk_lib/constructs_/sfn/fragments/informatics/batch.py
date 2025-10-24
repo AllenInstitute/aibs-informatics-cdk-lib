4bcda63251e6a9bb7861ca775e2d5078ca63aa98
Ryan McGinty <rpmcginty@users.noreply.github.com>
2025-10-23 22:21:57 -0700

    restricting iam policy statement

--- src/aibs_informatics_cdk_lib/constructs_/sfn/fragments/informatics/batch.py
+++ src/aibs_informatics_cdk_lib/constructs_/sfn/fragments/informatics/batch.py
@@ -49,6 +49,7 @@ class BatchInvokedBaseFragment(EnvBaseStateMachineFragment, EnvBaseConstructMixi
                 actions=["iam:PassRole"],
                 effect=iam.Effect.ALLOW,
                 resources=[f"arn:aws:iam::*:role/{self.env_base}*"],
+                conditions={"StringLike": {"iam:PassedToService": "batch.amazonaws.com"}},
             ),
         ]
 
